6c61a0c96b557edbb22d0fac98bfcd6fe682dab5
Steven Brown <S.Brown@physik.uni-muenchen.de>
2022-03-02 15:50:44 +0100

    plot in napari dock with button for calling a function

--- eda_napari/_widget.py
+++ eda_napari/_widget.py
@@ -1,22 +1,32 @@
-from napari.utils.notifications import show_infols
+
+from napari.utils.notifications import show_info
 
 import matplotlib.pyplot as plt
 from matplotlib.figure import Figure
 
 from matplotlib.backends.backend_qt5agg import FigureCanvas
 from qtpy.QtWidgets import QDialog, QWidget, QVBoxLayout
-
+import magicgui
 from magicgui import magic_factory
+from typing import Union
+import qtpy
+from qtpy.QtWidgets import QPushButton, QVBoxLayout
+import napari 
 
-#Widget = Union["magicgui.widgets.Widget", "qtpy.QtWidgets.QWidget"] UNION function?? tuto
+
+Widget = Union["magicgui.widgets.Widget", "qtpy.QtWidgets.QWidget"]
 
 
-class MyWidget(QDialog):
+class MyWidget(QWidget):
    def __init__(self, napari_viewer):
    #def __init__(self, viewer: 'napari.viewer.Viewer'):
       super().__init__()
       self._viewer = napari_viewer
+      self.run_function=QPushButton('Run function')
+      self.run_function.clicked.connect(self.plot_line)
+      self.layout=QVBoxLayout(self)
       self._init_mpl_widgets()
+      self.layout.addWidget(self.run_function)
         
    def _init_mpl_widgets(self):
       """Method to initialise a matplotlib figure canvas and the VoxelPlotter UI.
@@ -39,17 +49,23 @@ class MyWidget(QDialog):
                         size=15,
                         bbox=dict(facecolor=(0.9, 0.9, 0.9), alpha=1, boxstyle='square'))
 
-      # construct layout
-      layout = QVBoxLayout()
       # layout.addWidget(self.toolbar)
-      layout.addWidget(self.canvas)
-      self.setLayout(layout)
+      self.layout.addWidget(self.canvas)
       self.setWindowTitle('Voxel Plotter')
 
-from magicgui import magic_factory
+   def plot_line(self):
+      self.ax.plot([1,2,3,4])
+      self.fig.canvas.draw()
+      
 
-#@magic_factory what does this do? with it doesn't work for me
-def show_plot():
-   plt.plot([1, 2, 3, 4])
-   plt.ylabel('some numbers')
-   plt.show()
+from magicgui import magic_factory
+@magic_factory #what does this do? with it doesn't work for me
+def show_plot(
+    image: "napari.types.ImageData", threshold: int
+   ) -> "napari.types.LabelsData":
+    """Generate thresholded image.
+
+    This pattern uses magicgui.magic_factory directly to turn a function
+    into a callable that returns a widget.
+    """
+    return (image > threshold).astype(int)
