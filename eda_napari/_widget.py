84fd8682ea58d0affc6536a51a9674ddf38821cf
Steven Brown <S.Brown@physik.uni-muenchen.de>
2022-03-21 16:34:04 +0100

    Improved plots and code description

--- eda_napari/_widget.py
+++ eda_napari/_widget.py
@@ -1,94 +1,90 @@
 
 from napari.utils.notifications import show_info
-
+import matplotlib.style as style
+import os
+style.use(str(os.path.dirname(__file__))+'/plot_stylesheet.mplstyle') #get path of parent directory of script since plot_stylesheet is in the same directory
 import matplotlib.pyplot as plt
-from matplotlib.figure import Figure
-
 from matplotlib.backends.backend_qt5agg import FigureCanvas
-from qtpy.QtWidgets import QDialog, QWidget, QVBoxLayout
+from qtpy.QtWidgets import QWidget, QVBoxLayout
 import magicgui
 from magicgui import magic_factory
 from typing import Union
 import qtpy
-from qtpy.QtWidgets import QPushButton, QVBoxLayout
+from qtpy.QtWidgets import QVBoxLayout
 import napari
-
-
-
-#install tiff file reader plugin
-from napari_tifffile_reader import napari_get_reader
 import tifffile
 import xmltodict
-from functools import partial
 
-#import ctypes
-                                               
-
+#Union is a type: it forms the math union.
+#It means the widget could be a magicgui widget or a qtpy widget.                                             
 Widget = Union["magicgui.widgets.Widget", "qtpy.QtWidgets.QWidget"] 
-#Union is a type: it forms the math union. 
-#It means the widget could be a magicgui widget or a qtpy widget.
 
+class Frame_rate_Widget(QWidget):
+   """The Frame rate plotter widget.
 
-class MyWidget(QWidget):
+      This widget is a object inheriting from QWidget. Defining Frame_rate_Widget in the manifest 
+      "napari.yaml" allows Napari to reconise it's existance and display it in the plugin. Upon it's creation the __init__ function ensures
+      the execution of specific functions and plots the frame rates of an OME.tiff file inserted in the napari viewer.
+   """
+
    def __init__(self, napari_viewer):
-   #def __init__(self, viewer: 'napari.viewer.Viewer'):
+      """Constructor of the Frame_rate_Widget.
+      
+      This constructor initialises two blank canvases,the class's viewer to the napari viewer and connects events to functions to allow dynamic plots.
+      A newly inserted file or modification of slider position in napari causes the plot to update.
+      """
       super().__init__()
       self._viewer = napari_viewer
+      self.image_path=None
+      self.channel=0
       self._viewer.layers.events.inserted.connect(self.plot_frame_data)
-      self._viewer.dims.events.current_step.connect(self.plot_slider_position) # at the moment this causes an error
+      self._viewer.dims.events.current_step.connect(self.plot_slider_position) 
       self.layout=QVBoxLayout(self)
       self._init_mpl_widgets()
-      self.image_path=None
       try:
         self.image_path = self._viewer.layers[0].source.path #when MyWidget is activated it search for exisiting image
-      except (IndexError): # if no image is placed yet then Errors would occur
+      except (IndexError): # if no image is found then an index Error would occur
           pass
       
-      #self.run_button_times=QPushButton('Plot frame times')
-      #self.run_button_times.clicked.connect(self.plot_times)
-      #self.run_button_frame_r=QPushButton('Plot frame rate')
-      #self.run_button_frame_r.clicked.connect(lambda: self.plot_frame_rate('Hz')) #connect signal to a lamda to allow default arguments of function to be called
-      #self.layout.addWidget(self.run_button_times)
-      #self.layout.addWidget(self.run_button_frame_r)
 
    def _init_mpl_widgets(self):
-      """Method to initialise a matplotlib figure canvas and the VoxelPlotter UI.
+      """Method to initialise 2 matplotlib figure canvases with a basic layout and title.
 
       This method generates a matplotlib.backends.backend_qt5agg.FigureCanvas and populates it with a
-      matplotlib.figure.Figure and further matplotlib artists. The canvas is added to a QVBoxLayout afterwards.
+      matplotlib.pyplot.figure. The canvas is added to the QWidget Layout afterwards.
      """
-      # set up figure and axe objects
-      self.fig = Figure()
-      self.fig2 = Figure()
+      
+      self.fig = plt.figure()
+      self.fig2 =  plt.figure()
       self.canvas = FigureCanvas(self.fig)
       self.canvas2 = FigureCanvas(self.fig2)
       self.ax = self.fig.add_subplot(111)
       self.ax2 = self.fig2.add_subplot(111)
       self.layout.addWidget(self.canvas)
       self.layout.addWidget(self.canvas2)
-      self.setWindowTitle('Plot frame rate or frame times')
+      #self.setWindowTitle('Plot frame rate or frame times')
       try:
          self.plot_frame_data() #automatically plot frame data when Mywidget is called
       except(IndexError): # if no image is placed yet then Errors would occur when the source is retrieved
           pass
       
       
-
-   def plot_line(self):
-      self.ax.plot([1,2,3,4])
-      self.fig.canvas.draw()
-
-   def load_times(self,image_stack_path,channel=0):
-      """load frame times in ms from .ome.tif file for a certain channel"""
+   def get_times(self):
+      """Method that gets the capture time from the metadata.
+      
+      Input:-
+      Output: Vector of time metadata [ms] of images found at given image path and channel.
+      The times of each image stack from a ome.tif file is read in [ms] or [s] and then returned in [ms]. 
+      The times are taken from a given channel. The data can only be read from an ome.tif file. The Offset 
+      from time t=0 subrtracted to the times before it is returned."""
       times=[]
-      #print(self.image_path)
-      with tifffile.TiffFile(image_stack_path) as tif:
-         XML_metadata= tif.ome_metadata #returns an  OME XML file, without () invokes the function without calling it, calling it causes an error
+      with tifffile.TiffFile(self.image_path) as tif:
+         XML_metadata= tif.ome_metadata #returns a reference to a function that accesses the metadata as a OME XML file
          dict_metadata=xmltodict.parse(XML_metadata) #converts the xml to a dictionary to be readable
          num_pages=len(tif.pages) #the number of images stacked
          for frame in range(0,num_pages):
             #time should be in either s or ms
-            if float(dict_metadata['OME']['Image']['Pixels']['Plane'][frame]['@TheC'])==channel: #checks if correct channel
+            if float(dict_metadata['OME']['Image']['Pixels']['Plane'][frame]['@TheC'])==self.channel: #checks if correct channel
                frame_time_unit=dict_metadata['OME']['Image']['Pixels']['Plane'][frame]['@DeltaTUnit']
                if frame_time_unit== 's' :
                   convert_unit_to_ms=1000
@@ -99,29 +95,31 @@ class MyWidget(QWidget):
                else:
                   print('Time units not in ms or s but in '+ frame_time_unit+'. A conversion to ms or s must be done.')
       
+      times = [x - times[0] for x in times] #remove any offset from time=0
       return times
    
-   
-   def get_times(self,channel=0):
-      """This function gets frame capture times for a specific channel and starts with time=0"""
-      times=self.load_times(self.image_path,channel)
-      times = [x - times[0] for x in times] #start at time 0
-      return times
 
 
    def plot_times(self):
       self.ax.clear() #clear plot before plotting
       self.ax.set_ylabel('Time [ms]')
       self.ax.set_xlabel('Frame number')
-      self.ax.set_title('Evolution of elapsed time between frames')
+      self.ax.set_title('Evolution of elapsed time')
       times=self.get_times()
       self.ax.plot(times)
+      self.ax.ticklabel_format(axis='y', style='scientific', scilimits=(0,0), useMathText='True')
+      self.line_1=self.ax.axvline(0,0,1,linewidth=1, color='indianred')#initilaise a vertical line
       self.fig.canvas.draw()
 
    def get_frame_rate(self,unit_frame_r='Hz'):
-      """ The frame rate is calculate for each frame. Since the system is discrete it must be approximated.
-       For the first and last frame the last interval will be taken. For the others the avg of the previous 
-       and next interval will approximate the frame rate."""
+      """ Method that returns frame rate.
+
+      Input: unit of frame rate: [kHz] or [Hz]
+      Output: Vector of frame rates in [kHz] or [Hz]
+      
+      The frame rate is calculate for each frame. Since the system is discrete, it must be approximated.
+       For the first and last frame only one time interval will be taken. For the others, the average of the previous 
+       and next interval approximates the frame rate at a given point."""
       if unit_frame_r=='Hz':
          conversion_factor =1000     #convert to kHz to Hz
       elif unit_frame_r=='kHz':
@@ -146,24 +144,27 @@ class MyWidget(QWidget):
       self.ax2.set_xlabel('Frame number')
       self.ax2.set_title('Evolution of the Frame rate ')
       self.ax2.plot(frame_rate)
+      self.line_2=self.ax2.axvline(0,0,1,linewidth=1, color='indianred')#initialise a vertical line
       self.fig2.canvas.draw()
 
    
    def plot_frame_data(self):
-     
       self.image_path = self._viewer.layers[0].source.path #update image_path
       self.plot_times()
       self.plot_frame_rate()
-      self.line_1=self.ax.axvline(0,0,1,linewidth=1, color='k')
-      self.line_2=self.ax2.axvline(0,0,1,linewidth=1, color='k')
-
+      
+      
+      
    def plot_slider_position(self,event): #event information stored in "event"
+      """ Method plots and updates slider position on the canvas.
+
+      Input: event of current_step from slider
+      Output: -
+      After moving the slider on napari this function is called to update the vertical lines. The vertical
+      lines show the frame rate and capture time of the current image disaplayed napari viewer"""
       current_frame=event.source.current_step[0]
-      #times=self.get_times()
-      #frame_rate=self.get_frame_rate()
-      self.line_1.set_xdata(current_frame)
-      #self.line_1.set_ydata(current_frame)
-      self.line_2.set_xdata(current_frame)
+      self.line_1.set_xdata(current_frame) #update line
+      self.line_2.set_xdata(current_frame) #update line
       self.fig.canvas.draw()
       self.fig2.canvas.draw()
 
