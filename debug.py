bc927a6788f1679c5764713c4aa48acefd1ace2d
Steven Brown <S.Brown@physik.uni-muenchen.de>
2022-03-28 16:11:55 +0200

    Added small plot features and solved some plot issues

--- debug.py
+++ debug.py
@@ -1,7 +1,12 @@
 import napari
+from eda_napari import _widget
+
 
 
 with napari.gui_qt():  #what is this why can't open directly?
     viewer = napari.Viewer() 
     viewer.open("/Users/stevenbrown/software/images/napari_example2/steven_2_MMStack_Pos0.ome.tif")
-    #napari.components.ViewerModel.open()
+    widget=_widget.Frame_rate_Widget(viewer)
+    #dock_widget = viewer.window.add_dock_widget(widget, area='right')
+    #viewer.addDockWidget(qtpy.RightDockWidgetArea, _widget.Frame_rate_Widget(viewer)) #creates plots but not in napari
+    #napari.components.ViewerModel.open(viewer)
--- eda_napari/_widget.py
+++ eda_napari/_widget.py
@@ -10,10 +10,11 @@ import magicgui
 from magicgui import magic_factory
 from typing import Union
 import qtpy
-from qtpy.QtWidgets import QVBoxLayout
+from qtpy.QtWidgets import QVBoxLayout, QPushButton, QLabel
 import napari
 import tifffile
 import xmltodict
+from PyQt5.QtCore import Qt
 
 #Union is a type: it forms the math union.
 #It means the widget could be a magicgui widget or a qtpy widget.                                             
@@ -36,13 +37,27 @@ class Frame_rate_Widget(QWidget):
       super().__init__()
       self._viewer = napari_viewer
       self.image_path=None
+      self.time_data=None
+      self.frame_rate_data=None
       self.channel=0
+      self.frame_x_axis_time=True
       self._viewer.layers.events.inserted.connect(self.plot_frame_data)
       self._viewer.dims.events.current_step.connect(self.plot_slider_position) 
       self.layout=QVBoxLayout(self)
       self._init_mpl_widgets()
+      self.button_txt=('Time -> Frame number')
+      self.button_axis_change=QPushButton(self.button_txt)
+      self.button_axis_change.clicked.connect(self.change_axis)
+      self.layout.addWidget(self.button_axis_change)
+      self.frame_rate_label = QLabel("Frame rate = : ")
+      self.layout.addWidget(self.frame_rate_label)
+   
+     
+      
       try:
         self.image_path = self._viewer.layers[0].source.path #when MyWidget is activated it search for exisiting image
+        self.time_data=self.get_times()#init times of initial image
+        self.frame_rate_data=self.get_frame_rate()#init frame rate of initial image
       except (IndexError): # if no image is found then an index Error would occur
           pass
       
@@ -53,15 +68,11 @@ class Frame_rate_Widget(QWidget):
       This method generates a matplotlib.backends.backend_qt5agg.FigureCanvas and populates it with a
       matplotlib.pyplot.figure. The canvas is added to the QWidget Layout afterwards.
      """
-      
       self.fig = plt.figure()
-      self.fig2 =  plt.figure()
       self.canvas = FigureCanvas(self.fig)
-      self.canvas2 = FigureCanvas(self.fig2)
-      self.ax = self.fig.add_subplot(111)
-      self.ax2 = self.fig2.add_subplot(111)
+      self.ax = self.fig.add_subplot(211)
+      self.ax2 = self.fig.add_subplot(212)
       self.layout.addWidget(self.canvas)
-      self.layout.addWidget(self.canvas2)
       #self.setWindowTitle('Plot frame rate or frame times')
       try:
          self.plot_frame_data() #automatically plot frame data when Mywidget is called
@@ -105,10 +116,9 @@ class Frame_rate_Widget(QWidget):
       self.ax.set_ylabel('Time [ms]')
       self.ax.set_xlabel('Frame number')
       self.ax.set_title('Evolution of elapsed time')
-      times=self.get_times()
-      self.ax.plot(times)
+      self.ax.plot(self.time_data)
       self.ax.ticklabel_format(axis='y', style='scientific', scilimits=(0,0), useMathText='True')
-      self.line_1=self.ax.axvline(0,0,1,linewidth=1, color='indianred')#initilaise a vertical line
+      self.line_1=self.ax.axvline(self._viewer.dims.current_step[0],0,1,linewidth=1, color='indianred')#initilaise a vertical line
       self.fig.canvas.draw()
 
    def get_frame_rate(self,unit_frame_r='Hz'):
@@ -120,6 +130,7 @@ class Frame_rate_Widget(QWidget):
       The frame rate is calculate for each frame. Since the system is discrete, it must be approximated.
        For the first and last frame only one time interval will be taken. For the others, the average of the previous 
        and next interval approximates the frame rate at a given point."""
+     
       if unit_frame_r=='Hz':
          conversion_factor =1000     #convert to kHz to Hz
       elif unit_frame_r=='kHz':
@@ -127,33 +138,51 @@ class Frame_rate_Widget(QWidget):
       else:
          print('unit of frame rate not reconised: please use Hz or kHz')
 
-      times=self.get_times()
-      N_frames= len(times)
-      frame_rate=[conversion_factor/abs(times[1]-times[0])]#for the first frame rate no avg can be computed
+      N_frames= len(self.time_data)
+      frame_rate=[conversion_factor/abs(self.time_data[1]-self.time_data[0])]#for the first frame rate no avg can be computed
       for i in range(1,N_frames-1):
-         avg_rate=0.5*(1/abs(times[i+1]-times[i]) + 1/abs(times[i]-times[i-1]))
+         avg_rate=0.5*(1/abs(self.time_data[i+1]-self.time_data[i]) + 1/abs(self.time_data[i]-self.time_data[i-1]))
          frame_rate.append(conversion_factor*avg_rate)
-      frame_rate.append(conversion_factor/abs(times[1]-times[0])) #last frame rate
+      frame_rate.append(conversion_factor/abs(self.time_data[1]-self.time_data[0])) #last frame rate
 
       return frame_rate
    
    def plot_frame_rate(self,unit_frame_r='Hz'):
-      frame_rate=self.get_frame_rate()
+
       self.ax2.clear() #clear plot before plotting
       self.ax2.set_ylabel('Frame rate ['+unit_frame_r+']')
-      self.ax2.set_xlabel('Frame number')
+      self.ax2.ticklabel_format(axis='x', style='scientific', scilimits=(0,0), useMathText='True')
       self.ax2.set_title('Evolution of the Frame rate ')
-      self.ax2.plot(frame_rate)
-      self.line_2=self.ax2.axvline(0,0,1,linewidth=1, color='indianred')#initialise a vertical line
-      self.fig2.canvas.draw()
+      if self.frame_x_axis_time:
+         self.ax2.set_xlabel('Time [ms]')
+         self.ax2.plot(self.time_data,self.frame_rate_data)
+         vline_pos=self.time_data[self._viewer.dims.current_step[0]]
+         txt_text_box='Time of current frame = '+str(self.time_data[self._viewer.dims.current_step[0]])+'[ms]'
+      else:
+         self.ax2.set_xlabel('Frame number')
+         self.ax2.plot(self.frame_rate_data)
+         vline_pos=self._viewer.dims.current_step[0]
+         txt_text_box='Current frame number = '+str(self._viewer.dims.current_step[0])
+      
+
+      self.ax2.set_ylim(0,self.ax2.get_ylim()[1]*1.1) #increase plot for text space
+      self.line_2=self.ax2.axvline(vline_pos,0,1,linewidth=1, color='indianred')#initialise a vertical line
+      props = dict(boxstyle='round', facecolor='wheat', alpha=0.3)
+      self.text_box=self.ax2.text(0.05, 0.95, txt_text_box, transform=self.ax2.transAxes, fontsize=8, verticalalignment='top', bbox=props,color='red')
+      self.fig.canvas.draw()
 
    
    def plot_frame_data(self):
       self.image_path = self._viewer.layers[0].source.path #update image_path
+      self.time_data=self.get_times() #update time and frame rate data
+      self.frame_rate_data=self.get_frame_rate()
       self.plot_times()
       self.plot_frame_rate()
-      
-      
+      current_frame=self._viewer.dims.current_step[0]
+      if self.frame_x_axis_time:
+         self.line_2.set_xdata(self.time_data[current_frame])#update according to x axis (time or frame)
+      else: 
+         self.line_2.set_xdata(current_frame)
       
    def plot_slider_position(self,event): #event information stored in "event"
       """ Method plots and updates slider position on the canvas.
@@ -164,11 +193,28 @@ class Frame_rate_Widget(QWidget):
       lines show the frame rate and capture time of the current image disaplayed napari viewer"""
       current_frame=event.source.current_step[0]
       self.line_1.set_xdata(current_frame) #update line
-      self.line_2.set_xdata(current_frame) #update line
-      self.fig.canvas.draw()
-      self.fig2.canvas.draw()
+      if self.frame_x_axis_time:
+         self.line_2.set_xdata(self.time_data[current_frame])#update according to x axis (time or frame)
+         self.text_box.set_text('Time of current frame = '+str(self.time_data[current_frame])+'[ms]')
+      else: 
+         self.line_2.set_xdata(current_frame)
+         self.text_box.set_text('Current frame number = '+str(self._viewer.dims.current_step[0]))
 
-     
+      self.frame_rate_label.setText('Current frame rate: ' + str(self.frame_rate_data[current_frame])+'\n Time: '+str(self.time_data[current_frame]) +'\n Frame number: '+ str(current_frame))
+      self.fig.canvas.draw()
+      
+
+   def change_axis(self):
+      self.frame_x_axis_time=not self.frame_x_axis_time
+      if self.frame_x_axis_time:
+         button_txt='Time -> Frame number'
+         self.current_frame_txt='Current frame number = '
+         
+      else:
+         button_txt='Frame number -> Time'
+      self.button_axis_change.setText(button_txt)
+      self.plot_frame_data()
+
 
 from magicgui import magic_factory
 # decorate your function with the @magicgui decorator
