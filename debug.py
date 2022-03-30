e97813ff746313f764b5eff21dfb059cf1a8f790
Steven Brown <S.Brown@physik.uni-muenchen.de>
2022-03-30 16:09:08 +0200

    debug now starts widget

--- debug.py
+++ debug.py
@@ -7,6 +7,4 @@ with napari.gui_qt():  #what is this why can't open directly?
     viewer = napari.Viewer() 
     viewer.open("/Users/stevenbrown/software/images/napari_example2/steven_2_MMStack_Pos0.ome.tif")
     widget=_widget.Frame_rate_Widget(viewer)
-    #dock_widget = viewer.window.add_dock_widget(widget, area='right')
-    #viewer.addDockWidget(qtpy.RightDockWidgetArea, _widget.Frame_rate_Widget(viewer)) #creates plots but not in napari
-    #napari.components.ViewerModel.open(viewer)
+    viewer.window.add_plugin_dock_widget('eda-napari','Plot Frame rate')
