2ebc69e99d29ee0f4e542da0a24606987535fde8
Steven Brown <S.Brown@physik.uni-muenchen.de>
2022-04-04 21:31:18 +0200

    fixed double widget instance creation

--- debug.py
+++ debug.py
@@ -6,5 +6,4 @@ from eda_napari import _widget
 with napari.gui_qt():  #what is this why can't open directly?
     viewer = napari.Viewer() 
     viewer.open("/Users/stevenbrown/software/images/napari_example2/steven_2_MMStack_Pos0.ome.tif")
-    widget=_widget.Frame_rate_Widget(viewer)
     viewer.window.add_plugin_dock_widget('eda-napari','Plot Frame rate')
