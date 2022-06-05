c10998fa354d41e1bbb876ddc313b1116ed7139d
Steven Brown <S.Brown@physik.uni-muenchen.de>
2022-06-05 23:11:23 +0200

    use napari.run()

--- debug.py
+++ debug.py
@@ -1,10 +1,8 @@
 import napari
 import os
 
-
-
-with napari.gui_qt():  #what is this why can't open directly?
-    viewer = napari.Viewer() 
-    path=str(os.path.dirname(__file__))+'/images/example_image.tif'
-    viewer.open(path,plugin="aicsimageio-in-memory")
-    viewer.window.add_plugin_dock_widget('eda-napari','Plot frame rate') #'Add time scroller'
+viewer = napari.Viewer() 
+path=str(os.path.dirname(__file__))+'/images/example_image.tif'
+viewer.open(path,plugin="aicsimageio-in-memory")
+viewer.window.add_plugin_dock_widget('eda-napari','Plot frame rate') #'Add time scroller'
+napari.run()
