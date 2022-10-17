69690ec5b4d2f8f0fb21f4d994dec6be4f9ec906
Willi Stepp <willi.stepp@epfl.ch>
2022-10-17 13:28:45 +0200

    added remove layers

--- eda_napari/_widget.py
+++ eda_napari/_widget.py
@@ -5,7 +5,8 @@ from curses.panel import bottom_panel
 from napari.utils.notifications import show_info
 import os
 import pyqtgraph as qtplt
-from qtpy.QtWidgets import QWidget, QVBoxLayout, QHBoxLayout, QPushButton, QLabel, QGridLayout, QScrollBar, QDialog, QCheckBox, QLineEdit
+from qtpy.QtWidgets import QWidget, QVBoxLayout, QHBoxLayout, QPushButton, QLabel, QGridLayout, QScrollBar, QDialog, QCheckBox, QLineEdit, QSizePolicy
+from qtpy.QtCore import QSize
 import magicgui
 from magicgui import magic_factory
 from typing import Union
@@ -437,33 +438,43 @@ class Zarr_update_widget(QWidget):
       self._viewer = napari_viewer
       self.layout=QVBoxLayout(self)
       self.update_check = QCheckBox("Update data")
-      self.update_check.setChecked(True)
+      self.update_check.setChecked(False)
+      self.update_check.stateChanged.connect(self.update_check_changed)
       self.interval_label = QLabel("Update Interval [s]")
-      self.update_interval = QLineEdit('1')
+      self.update_interval = QLineEdit('10')
+      self.update_interval.editingFinished.connect(self.update_interval_value)
+      self.update_button = QPushButton("Update now")
+      self.update_button.clicked.connect(self.update)
       self.layout.addWidget(self.update_check)
       self.layout.addWidget(self.interval_label)
       self.layout.addWidget(self.update_interval)
+      self.layout.addWidget(self.update_button)
+
+
 
       self.layers_to_update = collections.defaultdict()
-
       self.current_sources = collections.defaultdict(lambda: collections.defaultdict(list))
 
       self._viewer.layers.events.inserted.connect(self.add_source)
-      self._viewer.layers.events.removed.connect(self.remove_layer)
+      self._viewer.layers.events.removing.connect(self.remove_layer)
+
+      self.setMaximumHeight(200)
 
       self.timer=QTimer()
-      self.timer.setInterval(1000)
+      self.timer.setInterval(10_000)
       self.timer.timeout.connect(self.update)
-      self.timer.start()
-
 
    def add_source(self, layer):
       reader = ome_zarr.reader.Reader(ome_zarr.io.parse_url(layer.source[-1].source.path))
       self.current_sources[layer.source[-1].source.path]['reader'] = reader
       self.current_sources[layer.source[-1].source.path]['layers'].append(layer.source[-1].name)
+      if all([os.path.exists(
+         os.path.join(os.path.dirname(layer.source[-1].source.path), "EDA", "nn_images")),
+              "EDA" not in [layer.name for layer in self._viewer.layers]]):
+         self._viewer.open(
+            os.path.join(os.path.dirname(layer.source[-1].source.path), "EDA"),
+            plugin='napari-ome-zarr', blending='additive', colormap='viridis')
       print(self.current_sources)
-      # list(reader())[0].data[0][:, 0, :, :, :]
-      print(layer.source[-1].source.path)
 
    def update(self):
       if self.update_check.isChecked():
@@ -473,8 +484,25 @@ class Zarr_update_widget(QWidget):
                self._viewer.layers[layer].data = list(source['reader']())[0].data[0][:, channel, :, :, :]
          print(time.perf_counter() - t0)
 
-   def remove_layer(self):
-      pass
+   def update_interval_value(self):
+      """Number changed in the GUI. Update the QTimer"""
+      self.timer.stop()
+      self.timer.setInterval(int(self.update_interval.text())*1000)
+      self.timer.start()
+
+   def update_check_changed(self):
+      print(self.timer.isActive())
+      if self.timer.isActive():
+         self.timer.stop()
+      else:
+         self.timer.start()
+
+   def remove_layer(self, layer):
+      self.current_sources[layer.source[-1].source.path]['layers'].remove(layer.source[-1].name)
+      # Check if list is empty and delete source completely
+      if not self.current_sources[layer.source[-1].source.path]['layers']:
+         del self.current_sources[layer.source[-1].source.path]
+
 
 class Time_scroller_widget(QWidget):
    """Time_scroller_widget class is a widget that creates a time scroll bar.  The scroll bar allows to animate stacks of
